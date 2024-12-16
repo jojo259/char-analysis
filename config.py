d54c1d986105d39a9e9572041e8df6423fc56d65
jojo259 <95320642+jojo259@users.noreply.github.com>
2024-12-16 14:45:50 +0000

    Initial commit

---
+++ config.py
@@ -0,0 +1,58 @@
+# corpus
+
+corpus_types = ["wikipedia"]#, "news", "newscrawl", "web"]
+corpus_years = range(2024, 2000, -1)  # from 2024 to 2001
+corpus_sizes = ["100K", "10K"]
+
+# analysis
+
+use_sentences_count = 100_000
+max_languages = 4
+assumed_text_length = 40
+extra_language_deweight = 1
+
+# general
+
+languages = [
+    "eng",
+    "deu",
+    "swe",
+    "dan",
+    "nor",
+    "nld",
+    "fry",
+    "isl",
+    "fao",
+    "ltz",
+    "pol",
+    "ces",
+    "slk",
+    "slv",
+    "fra",
+    "ita",
+    "spa",
+    "por",
+    "cat",
+    "glg",
+    "roh",
+    "cos",
+    "srd",
+    "ron",
+    "hun",
+    "fin",
+    "est",
+    "sme",
+    "cym",
+    "gle",
+    "bre",
+    "glv",
+    "tur",
+    "aze",
+    "lit",
+    "lav",
+    "eus",
+    "mlt",
+    "sqi",
+    "hsb",
+    "dsb",
+]
